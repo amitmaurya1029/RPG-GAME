22334c4ed197967cfcd1eb14e4a866ca03cde54c
AMIT <rs.mauryarocks@gmail.com>
2025-05-19 21:53:09 +0530

    Refactoring code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,8 +7,9 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
     NavMeshAgent agent;
+    private Animator animator;
 
-    [SerializeField] private Camera mainCamera;
+    private Camera mainCamera;
 
     void Awake()
     {
@@ -17,45 +18,61 @@ public class Mover : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-             
+        animator = transform.GetChild(0).GetComponent<Animator>();
+            
+
     }
 
-    
+
     void Update()
     {
-       
 
-
-         if (Input.GetMouseButtonDown(0))
-         {
+        if (Input.GetMouseButton(0))
+        {
             CreatRay();
-         }
+        }
 
-         
+        TrigerAnimation();
+
     }
 
 
     private void CreatRay()
     {
-         RaycastHit hit;
+        RaycastHit hit;
 
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, 1000))
         {
-            Debug.Log(" GET THE INTENDED POSITION FROM MOUSE : " +  hit.point);
+            Debug.Log(" GET THE INTENDED POSITION FROM MOUSE : " + hit.point);
 
-             if (agent != null)
+            if (agent != null)
             {
-                agent.destination = hit.point;
+                //agent.destination = hit.point;
+                MoveTo(hit.point);
             }
         }
 
 
-        Debug.DrawRay(ray.origin, ray.direction, Color.blue, 2 );
+        Debug.DrawRay(ray.origin, ray.direction, Color.blue, 2);
     }
 
 
+    public void MoveTo(Vector3 destination)
+    {
+         agent.destination = destination;
+    }
+
+    private void TrigerAnimation()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        animator.SetFloat("Velocity", localVelocity.z);
+
+        Debug.Log("player agent velocity : " + velocity);
+
+    }
 
 
 }
